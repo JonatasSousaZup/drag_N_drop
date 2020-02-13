36d75987654f80b4a61fdd39ecaf6ef43c7f8d48
Jonatas Oliveira <Jonatas.sousa@zup.com.br>
2020-02-13 14:22:44 -0300

    rocketfy

---
+++ src/components/Card/index.js
@@ -0,0 +1,60 @@
+import React, { useRef, useContext } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+import BoardContext from '../Board/context'
+
+import { Container, Label } from './styles'
+
+export default function Card({ data, index, listIndex }) {
+    const ref = useRef();
+    const { move } = useContext(BoardContext);
+
+    const [{ isDragging }, dragRef] = useDrag({
+        item: { type: 'CARD', index, listIndex },
+        collect: monitor => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
+
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor) {
+            const draggedListIndex = item.listIndex;
+            const targetListIndex = listIndex;
+            const draggedIndex = item.index;
+            const targetindex = index;
+
+            if (draggedIndex === targetindex && draggedListIndex === targetListIndex) {
+                return;
+            }
+            const targetSize = ref.current.getBoundingClientRect();
+            const targetCenter = targetSize.bottom - targetSize.top / 2;
+            const draggedOffset = monitor.getClientOffset();
+            const draggedTop = draggedOffset.y - targetSize.top;
+
+            if (draggedIndex < targetindex && draggedTop < targetCenter) {
+                return;
+            }
+
+            if (draggedIndex > targetindex && draggedTop > targetCenter) {
+                return;
+            }
+            move(draggedListIndex, targetListIndex, draggedIndex, targetindex)
+
+            item.index = targetindex;
+            item.listIndex = targetListIndex;
+        }
+    });
+
+    dragRef(dropRef(ref));
+
+    return (
+        <Container ref={ref} isDragging={isDragging}>
+            <header>
+                {data.labels.map(label => <Label color={label} />)}
+            </header>
+            <p>{data.content}</p>
+            {data.user && <img src={data.user} alt="" />}
+        </Container>
+    );
+}
